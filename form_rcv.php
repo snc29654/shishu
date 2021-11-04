ca0866ba06aa90965d09f0d705ec57666c12ee82
snc29654 <snc29654@nifty.com>
2021-11-04 12:47:16 +0900

    色の追加

--- form_rcv.php
+++ form_rcv.php
@@ -1,18 +1,31 @@
- <input type="radio" id="xxxx" name="fruit" value="black" onchange="func1()"
+<input type="radio" id="xxxx" name="fruit" value="brown" onchange="func1()"
  checked>
-  <label for="xxxx">黒</label><br>
+  <label for="xxxx">茶</label>
+
+  <input type="radio" id="xxxx" name="fruit" value="gray" onchange="func1()"
+ checked>
+  <label for="xxxx">灰</label>
+
+  <input type="radio" id="xxxx" name="fruit" value="pink" onchange="func1()"
+ checked>
+  <label for="xxxx">桃</label>
+
+
+<input type="radio" id="xxxx" name="fruit" value="black" onchange="func1()"
+ checked>
+  <label for="xxxx">黒</label>
   <input type="radio" id="xxxx" name="fruit" value="white" onchange="func1(
 )">
-  <label for="xxxx">白</label><br>
+  <label for="xxxx">白</label>
   <input type="radio" id="xxxx" name="fruit" value="green" onchange="func1(
 )">
-  <label for="xxxx">緑</label><br>
+  <label for="xxxx">緑</label>
 <input type="radio" id="xxxx" name="fruit" value="red" onchange="func1()"
  checked>
-  <label for="xxxx">赤</label><br>
+  <label for="xxxx">赤</label>
   <input type="radio" id="xxxx" name="fruit" value="blue" onchange="func1(
 )">
-  <label for="xxxx">青</label><br>
+  <label for="xxxx">青</label>
   <input type="radio" id="xxxx" name="fruit" value="yellow" onchange="func1(
 )">
   <label for="xxxx">黄</label><br>
@@ -21,7 +34,7 @@
     var fruits = document.getElementsByName("fruit");
     for(var i = 0; i < fruits.length; i++){
       if(fruits[i].checked) {
-color=fruits[i].value;
+        color=fruits[i].value;
         
       }
     }
